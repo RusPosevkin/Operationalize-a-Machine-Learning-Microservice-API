fc98db3287feaf15f02e651cda2376375f85eeb4
Ruslan Posevkin <rus.posevkin@gmail.com>
2020-02-29 20:40:57 +0300

    update upload_docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="rusposevkin/udacity-api-cloud"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag apidev $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
