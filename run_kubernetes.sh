31f4d117d941ef29e8fe3ce58462d0b1feba858c
Ruslan Posevkin <rus.posevkin@gmail.com>
2020-02-29 21:05:44 +0300

    done – Task 6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="rusposevkin/udacity-api-cloud"
+k8s_name="apidev"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run $k8s_name --generator=run-pod/v1 --image=$dockerpath --port=80 --labels app=$k8s_name
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward $k8s_name 8000:80
 
